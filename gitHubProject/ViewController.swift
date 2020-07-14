705f8ab7d6d8a1eeecd409b5739205f48fbcc135
Danielle <danielle.soltz@mjbha.org>
2020-07-14 10:46:44 -0400

    Initial Commit

---
+++ gitHubProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubProject
+//
+//  Created by Danielle Soltz on 7/14/20.
+//  Copyright © 2020 Danielle Soltz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
